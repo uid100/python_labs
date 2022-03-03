8cf2e21f8dc26df9749e5c3ceb4acfbdc336f7d1
Dan Sullivan <daniel.f.sullivan@gmail.com>
2022-03-02 21:04:11 -0800

    Update dice.py

--- 05_Arrays/dice.py
+++ 05_Arrays/dice.py
@@ -12,35 +12,35 @@ MAX_DICE_VAL = 6
 roll_types = ["Junk", "Pair", "3 of a kind", "4 of a kind", "5 of a kind"]
 
 # declare a list for dice rolls and tallying dice rolls
-pdice = [0,0,0,0,0]  # player's dice
-cdice = [0,0,0,0,0]  # computer's dice
+pdice = [0, 0, 0, 0, 0]  # player's dice
+cdice = [0, 0, 0, 0, 0]  # computer's dice
 
 # tally lists will be 6 in size since the dice values run from 1 - 6
-ptally = [0,0,0,0,0,0]  # player: how many of each value rolled
-ctally = [0,0,0,0,0,0]  # computer:  
+ptally = [0, 0, 0, 0, 0, 0]  # player: how many of each value rolled
+ctally = [0, 0, 0, 0, 0, 0]  # computer:
 
 
 # Start the game!
 # roll the dice for player and computer
 i = 0    # start the counter
 while i < HOW_MANY_DICE:
-    pdice[i] = random.randint(1,MAX_DICE_VAL)
-    cdice[i] = random.randint(1,MAX_DICE_VAL)
+    pdice[i] = random.randint(1, MAX_DICE_VAL)
+    cdice[i] = random.randint(1, MAX_DICE_VAL)
     i += 1
-    
+
 i = 0    # reset the counter
-print("Player rolled: ", end=" ")
+print("Player dice: ", end=" ")
 while i < HOW_MANY_DICE:
     print(pdice[i], end=" ")
     i += 1
 
 i = 0    # reset the counter
-print("Computer rolled: ", end=" ")
+print("\nComputer dice: ", end=" ")
 while i < HOW_MANY_DICE:
     print(cdice[i], end=" ")
     i += 1
-    
-    
+
+
 # Check the results!
 # Load the tally list so we can determine pair, 3 of a kind...
 # Notice here how we use the dice value (1 - 6) minus 1 as an array subscript (0 - 5). Pretty clever!
@@ -48,10 +48,10 @@ while i < HOW_MANY_DICE:
 i = 0
 while i < HOW_MANY_DICE:
     # if the value of this one is '1', then tally it in the first spot (0), if it's 6, add it to index 5.
-    ptally[ pdice[i] - 1 ] += 1
-    ctally[ pdice[i] - 1 ] += 1
+    ptally[pdice[i] - 1] += 1
+    ctally[cdice[i] - 1] += 1
     i += 1
-    
+
 # find the largest count
 # find out how many 2s, 3s, 4s... to determine pair, 2 of a kind...
 pmax = ptally[0]
@@ -69,8 +69,9 @@ while i < MAX_DICE_VAL:   # this should be   i < len(ptally) !
 
 # output - display pair, 3 of a kind...
 # Once again, we use an array (list) to replace a selection structure
-print("\nPlayer rolled: " + roll_types[pmax = 1])
-print("Computer rolled: " + roll_types[pmax = 1])
+print("\n\nPlayer rolled: " + roll_types[pmax - 1])
+print("Computer rolled: " + roll_types[cmax - 1])
+print()
 
 # who won?
 if pmax > cmax:
